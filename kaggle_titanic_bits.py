99d0e9d626189345af69f1b8e4f7338db1e97bdb
Somabha Bhattacharjya <somabho@gmail.com>
2016-02-05 13:22:16 +0900

    Update kaggle_titanic_bits.py

--- kaggle_titanic_bits.py
+++ kaggle_titanic_bits.py
@@ -27,3 +27,8 @@ sns.factorplot(x='Pclass',y='Survived',hue='person',data=titanic_df)
 
 sns.factorplot(x='Pclass',y='Age',hue='person',data=titanic_df)
 
+sns.factorplot(x='person',y='Survived', col='Pclass', data=titanic_df,saturation=.5,
+...                    kind="bar", ci=None, aspect=.6).set_xticklabels(["Men","Women","Children"]).set_titles("{col_name}{col_var}")
+
+
+
